f8083baea6603de594b4792816866fb3b4785281
Sarah Stenfert <sarah.stenfert@gmail.com>
2026-04-22 13:55:40 +1200

    Fixed a test failure. A test step had changed. Updated the test step after checking the change to the link text was intentional.

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -1,12 +1,8 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('test', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-  await page.getByRole('link', { name: 'Playwright logo Playwright' }).click();
-  await page.getByText('Community').click();
-  await page.getByText('Community').click();
-  await page.getByRole('link', { name: 'Get started' }).click();
-  await page.getByRole('link', { name: 'Setting up CI' }).click();
-  await page.getByRole('link', { name: 'Python' }).click();
-  await page.getByRole('link', { name: 'How to set up GitHub Actions' }).click();
+test("test", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
+  await page.getByRole("link", { name: "Playwright logo" }).click();
+  await page.getByRole("link", { name: "Get started" }).click();
+  await page.getByRole("link", { name: "How to install Playwright" }).click();
 });
