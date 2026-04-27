5085da1a0d8101236211816872715200ab6a4506
Sarah Stenfert <sarah.stenfert@gmail.com>
2026-04-28 11:10:04 +1200

    Uncommented the setup project code so the authentication state for a logged-in user is saved (as a json file) for re-use. Amazing functionality! Also refactored the home page test so it is now in a describe 'Home page' block containing individual tests rather than a long list of steps and checks.

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,13 +1,35 @@
 import { test, expect } from "@playwright/test";
 
-test("Home page", async ({ page }) => {
-  await page.goto("https://practicesoftwaretesting.com/");
-  // Ensure the sign-in link is present
-  await expect(page.getByTestId("nav-sign-in")).toContainText(/sign in/i);
+test.describe("Home page", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("https://practicesoftwaretesting.com/");
+  });
 
-  // Check the title of the page
+  test("Check sign in", async ({ page }) => {
+    // Ensure the sign-in link is present
+    await expect(page.getByTestId("nav-sign-in")).toContainText(/sign in/i);
+  });
 
-  // Check the count of items displayed
+  test("Validate page title", async ({ page }) => {
+    await expect(page).toHaveTitle(
+      "Practice Software Testing - Toolshop - v5.0",
+    );
+  });
 
-  // Search for Thor Hammer and check result
+  test("Check the count of items displayed", async ({ page }) => {
+    const productGrid = page.locator(".col-md-9");
+    await expect(productGrid.getByRole("link")).toHaveCount(9);
+    expect(await productGrid.getByRole("link").count()).toBe(9);
+  });
+
+  test("Search for Thor Hammer and check result", async ({ page }) => {
+    // Will remove duplicate variable later
+    const productGrid = page.locator(".col-md-9");
+    await page.getByTestId("search-query").fill("Thor Hammer");
+    await page.getByTestId("search-submit").click();
+    // I think checking the count could make the test flaky, so just leaving
+    // the code in for future reference
+    // await expect(productGrid.getByRole("link")).toHaveCount(7);
+    await expect(page.getByAltText(/thor hammer/i)).toBeVisible();
+  });
 });
