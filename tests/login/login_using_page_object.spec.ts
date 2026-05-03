6ce70da216e3c4e0363fc11451201ee341ca5a9f
Sarah Stenfert <sarah.stenfert@gmail.com>
2026-05-04 10:49:52 +1200

    Renamed and refactored the login using page object test file to improve readabilty. I.e. Used the login page login function instead of 3 explicit steps. And changed the login without page object test locators to use the test id more clearly.

---
+++ tests/login/login_using_page_object.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from "@playwright/test";
+import { LoginPage } from "../../pages/login/loginPage";
+
+test("Login without page object", async ({ page }) => {
+  await page.goto("http://practicesoftwaretesting.com/");
+  await page.getByTestId("nav-sign-in").click();
+  await page.getByTestId("email").fill("customer@practicesoftwaretesting.com");
+  await page.getByTestId("password").fill("welcome01");
+  await page.getByTestId("login-submit").click();
+  await expect(page.getByTestId("nav-menu")).toContainText("Jane Doe");
+  await expect(page.getByTestId("page-title")).toContainText("My account");
+});
+
+test("Login with page object", async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  await loginPage.goto();
+  await loginPage.login("customer@practicesoftwaretesting.com", "welcome01");
+  await expect(page.getByTestId("nav-menu")).toContainText("Jane Doe");
+});
