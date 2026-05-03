5d74ff096f4460e1abd35fbebec1139d3dbb7896
Sarah Stenfert <sarah.stenfert@gmail.com>
2026-05-04 10:44:49 +1200

    Refactored the baseUrl in the login test to make it clearer

--- pages/login/loginPage.ts
+++ pages/login/loginPage.ts
@@ -1,5 +1,7 @@
 import { type Locator, type Page } from "@playwright/test";
 
+const baseUrl = "https://practicesoftwaretesting.com";
+
 export class LoginPage {
   readonly page: Page;
   readonly emailInput: Locator;
@@ -13,7 +15,7 @@ export class LoginPage {
   }
 
   async goto() {
-    await this.page.goto(process.env.URL + "/auth/login");
+    await this.page.goto(baseUrl + "/auth/login");
   }
 
   async login(email: string, password: string) {
