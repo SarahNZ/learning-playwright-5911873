68188ee619456c43f29b40e003a18fa2bdad833b
Sarah Stenfert <sarah.stenfert@gmail.com>
2026-05-04 10:25:44 +1200

    Deleted practice test I don't need anymore

--- tests/test-1.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-test("test", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
-  await page.getByRole("link", { name: "Playwright logo" }).click();
-  await page.getByRole("link", { name: "Get started" }).click();
-  await page.getByRole("link", { name: "How to install Playwright" }).click();
-});
